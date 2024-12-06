bb5588886428c1825f59963862ff1f04fef4ce05
Saraswathi25 <saras.sapna25@gmail.com>
2024-12-05 20:51:29 -0500

    todo final

--- reactTodo/src/components/Todo.jsx
+++ reactTodo/src/components/Todo.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./fOOTER.JSX";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done == true).length;
+  const total = todos.length;
   return (
     <div>
       <Header />
       <Form todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} />
+      {todos.length > 0 && <TodoList todos={todos} setTodos={setTodos} />}
+      <Footer completedTodos={completedTodos} total={total} />
     </div>
   );
 }

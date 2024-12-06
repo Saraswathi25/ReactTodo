5bcef5561d59b23475548acf99160792b0710544
Saraswathi25 <saras.sapna25@gmail.com>
2024-12-05 20:55:29 -0500

    final

--- reactTodo/src/components/Todo.jsx
+++ reactTodo/src/components/Todo.jsx
@@ -10,10 +10,12 @@ export default function Todo() {
   const completedTodos = todos.filter((todo) => todo.done == true).length;
   const total = todos.length;
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Form todos={todos} setTodos={setTodos} />
-      {todos.length > 0 && <TodoList todos={todos} setTodos={setTodos} />}
+      <main className="flex-grow">
+        <Form todos={todos} setTodos={setTodos} />
+        {todos.length > 0 && <TodoList todos={todos} setTodos={setTodos} />}
+      </main>
       <Footer completedTodos={completedTodos} total={total} />
     </div>
   );
